06cf90dfc5a85450c78b140c605beed1c13e94a2
niuxinxin123 <niuxinxinn@163.com>
2019-07-08 15:38:26 +0800

    xiuxinxnin
    Signed-off-by: niuxinxin123 <niuxinxinn@163.com>

---
+++ src/components/home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="div">
+        111
+    </div>
+</template>
+
+<script>
+export default {
+  name:"App",
+ data(){
+     return{
+
+     }
+ },
+ mounted(){
+
+ },
+ methods:{
+     
+ }
+}
+</script>
+
+<style>
+
+</style>
